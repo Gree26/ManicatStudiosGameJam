c18263caf141f8b135374a1a6b2f26d0da2c8c67
Andrew R Spangenberg <74675198+Gree26@users.noreply.github.com>
2023-05-15 04:08:24 -0400

    Built a large part of the Pause menu UI

--- JellyPlayground-Manicat Studios/Assets/Scripts/CameraController.cs
+++ JellyPlayground-Manicat Studios/Assets/Scripts/CameraController.cs
@@ -9,6 +9,12 @@ public class CameraController : MonoBehaviour
 
     [SerializeField]
     private Vector3 _offset = Vector3.zero;
+
+    [SerializeField]
+    private float _verticalSpeed = 1f;
+
+    [SerializeField]
+    private float _horizontalSpeed = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,8 +29,8 @@ public class CameraController : MonoBehaviour
     private void NewMouseInput(Vector2 mouseValue)
     {
         Vector3 newRotationValue = new Vector3(mouseValue.y, mouseValue.x * -1, 0);
-        float newXRotation = transform.eulerAngles.x - newRotationValue.x;
-        transform.eulerAngles = new Vector3(newXRotation, transform.eulerAngles.y - newRotationValue.y, 0);
+        float newXRotation = transform.eulerAngles.x - (newRotationValue.x * _verticalSpeed);
+        transform.eulerAngles = new Vector3(newXRotation, transform.eulerAngles.y - newRotationValue.y * _horizontalSpeed , 0);
 
         InputHandler.instance.MoveDirection = new Vector3(0, this.transform.eulerAngles.y, 0);
     }
