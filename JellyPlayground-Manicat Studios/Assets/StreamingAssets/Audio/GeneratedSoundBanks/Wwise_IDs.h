395ea4d6f786af1ca043e746e6cad7954cb6f6db
Prismo <willclarkmusic@gmail.com>
2023-05-14 18:33:17 -0700

    Muisc_Implemented
    Music files imported
    Switch container made
    Unity implemented
    No state triggers

---
+++ JellyPlayground-Manicat Studios/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,59 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_MUSICMAIN = 3170845728U;
+        static const AkUniqueID STOP_MUSICMAIN = 1408954334U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSIC_STATES
+        {
+            static const AkUniqueID GROUP = 1690668539U;
+
+            namespace STATE
+            {
+                static const AkUniqueID END = 529726532U;
+                static const AkUniqueID LOADING = 3573931707U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID PAUSEMENUMUSIC = 2478971905U;
+                static const AkUniqueID SEC1 = 585140289U;
+                static const AkUniqueID SEC2 = 585140290U;
+                static const AkUniqueID TITLEMENUMUSIC = 508769081U;
+            } // namespace STATE
+        } // namespace MUSIC_STATES
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MUSICBANK = 3017428748U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
