e213201441e7f76bc47d8611c6ccf8a1c1dde61d
Prismo <willclarkmusic@gmail.com>
2023-05-16 14:33:46 -0700

    SoccerGame v1

--- JellyPlayground-Manicat Studios/Assets/Soccer/SoccerBall.cs
+++ JellyPlayground-Manicat Studios/Assets/Soccer/SoccerBall.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class SoccerBall : MonoBehaviour
 {
-    public float kickForce = 10f;
+
+    public GameObject goal;
+    public ParticleSystem goalParticle;
+    public float kickForce;
 
     private void OnCollisionEnter(Collision collision)
     {
-        // Check if the collision is with the player
         if (collision.gameObject.CompareTag("Player"))
         {
             Rigidbody ballRigidbody = GetComponent<Rigidbody>();
-
-            // Calculate the kick direction based on the collision normal
             Vector3 kickDirection = collision.contacts[0].normal;
-
-            // Apply the kick force to the ball in the kick direction
             ballRigidbody.AddForce(kickDirection * kickForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == goal)
+        {
+            goalParticle.Play();
         }
     }
 }
